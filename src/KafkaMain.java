dc5d14d224ff91410d27e1dd651b82dc62a138aa
sbartkowski <stanislawbartkowski@gmail.com>
2019-04-12 16:31:49 +0200

    Different modes to read Kafka topic: as a whole or by partition

--- src/KafkaMain.java
+++ src/KafkaMain.java
@@ -1,11 +1,18 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -15,6 +22,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -26,7 +34,6 @@ public class KafkaMain {
 	private static final String PRODUCE = "produce";
 	private static final String TOPIC = "topic";
 	private static final String CONSUME = "consume";
-	
 
 	private static void listTopics(Properties props) {
 		Map<String, List<PartitionInfo>> topics;
@@ -74,6 +81,78 @@ public class KafkaMain {
 		}
 	}
 
+// ============================================================
+// read by partitions, more complicated
+// allows moving offset to the end and ignore old messages
+// ============================================================
+
+	static class ReadPartition implements Callable<Void> {
+
+		
+		private final TopicPartition pinfo;
+		private final Properties prop;
+		
+		ReadPartition(Properties prop,String topic,int partitioninfo) {
+			pinfo = new TopicPartition(topic,partitioninfo);
+			this.prop = prop;
+		}
+		
+		@Override
+		public Void call() throws Exception {
+			try (Consumer<Long, String> consumer = new KafkaConsumer<>(prop)) {
+				consumer.assign(Arrays.asList(pinfo));
+				consumer.seekToEnd(Arrays.asList(pinfo));
+				System.out.println("Read for partition " + pinfo.partition());
+				while (true) {
+					ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
+					consumerRecords.forEach(record -> {
+						System.out.println(pinfo.partition() + "   " + record.key() + " " + record.value());
+					});
+				}
+			}			
+		}
+
+	}
+	
+	private static void consumeLinesByPartitions(Properties props, String topic)
+			throws InterruptedException, ExecutionException {
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		P("I'm consuming lines from topic " + topic + " unless stopped by CTRL/C ...");
+		Collection<PartitionInfo> pList;
+		try (Consumer<Long, String> consumer = new KafkaConsumer<>(props)) {
+			pList = consumer.partitionsFor(topic);
+		}
+		ExecutorService e = Executors.newCachedThreadPool();
+		pList.forEach(t -> e.submit(new ReadPartition(props,topic,t.partition())));
+		e.shutdown();
+		e.awaitTermination(30,TimeUnit.MINUTES);		
+	}
+	
+	// ======================
+	// another version
+	// ======================
+
+	private static void consumeLinesByPartitonsPrevious(Properties props, String topic)
+			throws InterruptedException, ExecutionException {
+		P("I'm consuming lines from topic " + topic + " unless stopped by CTRL/C ...");
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		try (Consumer<Long, String> consumer = new KafkaConsumer<>(props)) {
+			Collection<PartitionInfo> tlist = consumer.partitionsFor(topic);
+			Collection<TopicPartition> plist = tlist.stream().map(t -> new TopicPartition(topic, t.partition()))
+					.collect(Collectors.toList());
+			consumer.assign(plist);
+			consumer.seekToEnd(plist);
+			while (true) {
+				ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
+				consumerRecords.forEach(record -> {
+					System.out.println(record.key() + " " + record.value());
+				});
+			}
+		}
+	}
+
 	private static void P(String s) {
 		System.out.println(s);
 	}
@@ -101,7 +180,8 @@ public class KafkaMain {
 		else if (PRODUCE.equals(args[1]))
 			produceLines(props, topic);
 		else if (CONSUME.equals(args[1]))
-			consumeLines(props, topic);
+//			consumeLines(props, topic);
+			consumeLinesByPartitions(props,topic);
 		else
 			printHelp();
 	}
